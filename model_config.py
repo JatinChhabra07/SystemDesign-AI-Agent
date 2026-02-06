15e872158d8ee5b4fbff16e77c4b807e0c0ea6b6
JatinChhabra07 <chhabrajatin0073@gmail.com>
2026-02-06 13:06:50 +0530

    added model config file

---
+++ model_config.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+
+load_dotenv()
+
+def get_model():
+    provider = os.getenv("MODEL_PROVIDER","groq")
+    if provider=="groq":
+        return ChatGroq(
+            model="llama-3.3-70b",
+            api_key=os.getenv("GROQ_API_KEY")
+        )
+    else:
+        raise ValueError("Invalid MODEL_PROVIDER")
