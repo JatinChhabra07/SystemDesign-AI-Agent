ab4b384c6876d4265b805493b1bcdf3abeb0dab3
JatinChhabra07 <chhabrajatin0073@gmail.com>
2026-02-10 13:26:49 +0530

    added RAG logic

--- backend/src/agents/researcher.py
+++ backend/src/agents/researcher.py
@@ -1,19 +1,30 @@
 from langchain_community.tools.tavily_search import TavilySearchResults
 from dotenv import load_dotenv
+from src.utils.vector_store import ingest_docs
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
 load_dotenv()
 
-
-search_tool= TavilySearchResults(max_results=3)
-
 def research_agent(state):
-    """Executes the research step of the plan."""
+    """Executes the research step of the plan, checking local PDFs first to save costs."""
+
+    embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    vectorstore = Chroma(persist_directory="backend/data/chroma_db", embedding_function=embeddings)
 
     query = state["plan"].steps[state["current_step"]].task_description
-
-    search_results = search_tool.invoke({"query": query})
+    local_docs = vectorstore.similarity_search(query, k=2)
 
     # Append the findings to the message history
-    content = f"Research Findings for '{query}':\n\n{search_results}"
+    if local_docs:
+        results_source = "Local PDF Documents"
+        search_results = "\n".join([doc.page_content for doc in local_docs])
+    
+    else:
+        results_source = "Web Search (Tavily)"
+        search_tool= TavilySearchResults(max_results=3)
+        search_results = search_tool.invoke({"query": query})
+
+    content = f"Source: {results_source}\nResearch Findings for '{query}':\n\n{search_results}"
     return {
         "messages": [{"role": "assistant", "content": content}],
         "current_step": state["current_step"] + 1
---
+++ backend/src/utils/vector_store.py
@@ -0,0 +1,28 @@
+import os
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+
+DATA_PATH = "backend/data/"
+DB_PATH = "backend/data/chroma_db"
+
+
+def ingest_docs(file_path:str):
+    """Loads a PDF, splits it, and saves it to a local vector DB."""
+
+    loader = PyPDFLoader(file_path)
+    docs = loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap=100)
+    splits = text_splitter.split_documents(docs)
+
+    embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+
+    vectorstore = Chroma.from_documents(
+        documents=splits,
+        embedding=embeddings,
+        persist_directory=DB_PATH
+    )
+
+    return vectorstore.as_retriever()
