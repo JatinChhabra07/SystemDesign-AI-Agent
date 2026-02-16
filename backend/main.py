b1de376df562b8d97e0d2f069e1c5f3b102487fd
JatinChhabra07 <chhabrajatin0073@gmail.com>
2026-02-16 23:28:56 +0530

    changed main file

--- backend/main.py
+++ backend/main.py
@@ -169,6 +169,6 @@ async def generate_design(request: QueryRequest):
 
     
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    render_port = int(os.environ.get("PORT", 8000)) 
+    uvicorn.run(app, host="0.0.0.0", port=render_port)
         
